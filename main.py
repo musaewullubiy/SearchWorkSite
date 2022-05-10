46111d87d996930e581f675d5e1ed12f20ae80af
user <grasjesdraste@yandex.ru>
2022-05-11 02:46:04 +0300

    path updated to heroku

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ from data.appointments import Appointments
 
 from data import db_session
 
-PATH = 'http://127.0.0.1:5000'
+PATH = 'https://searchwork.herokuapp.com'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
